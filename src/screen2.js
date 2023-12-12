0a67fb5df1898199b37e365f3ff1467b2389b830
nickhaughton <nick@openindsutry.io>
2023-12-11 21:29:29 -0500

    wip

--- src/screen2.js
+++ src/screen2.js
@@ -101,60 +101,131 @@ const Main = () => {
     );
   }
 
+  let description, contents;
+
+  if (task === "color") {
+    description = (
+      <span className=" text-pnpl">
+        If you could change the color of the sky and the grass to{" "}
+        <span className="font-semibold">how you felt today</span> how would it
+        look like?
+      </span>
+    );
+    contents = (
+      <div className="">
+        <div className="rounded-t-2xl flex flex-col space-y-4">
+          <div
+            style={{
+              backgroundColor: colors?.skyColor
+                ? buildRGBA(colors.skyColor)
+                : "#fff",
+            }}
+          >
+            <img className="w-full" src="/sky2.png" />
+          </div>
+          <div className="" style={{ marginTop: "-25px" }}>
+            <Grass
+              color={colors?.grassColor ? buildRGBA(colors.grassColor) : "#fff"}
+            />
+          </div>
+
+          <>
+            <p className="text-pnpl text-sm">
+              Use the slider below to make your selection
+            </p>
+
+            <Tabs
+              onColorChange={(colors) => {
+                setColors(colors);
+              }}
+              skyColor={colors?.skyColor}
+              grassColor={colors?.grassColor}
+            />
+          </>
+        </div>
+      </div>
+    );
+  } else if (task === "video") {
+    description = (
+      <span className=" text-pnpl">
+        Let’s Dance! Tap “Allow Camera” to animate an avatar that captures your
+        unique dance moves:
+      </span>
+    );
+
+    contents = (
+      <div className="">
+        <div className="rounded-t-2xl flex flex-col space-y-4">
+          <div className="flex flex-col justify-center items-center relative">
+            {/* <P5Sketch
+              isRecording={recordingState === "recording"}
+              recordingComplete={(blob) => {
+                setBlob(blob);
+              }}
+            /> */}
+
+            {recordingState === "none" && <img src="/reference.png" />}
+            {recordingState === "none" && (
+              <div className="absolute">
+                <div className="w-24 h-24" onClick={handlePlay}>
+                  <PlayButton />
+                </div>
+              </div>
+            )}
+            {recordingState === "started" && (
+              <div className="absolute">
+                <CountdownCircleTimer
+                  isPlaying
+                  duration={5}
+                  colors={["#fff"]}
+                  onComplete={() => {
+                    setRecordingState("recording");
+                  }}
+                  size={100}
+                  width={2}
+                >
+                  {({ remainingTime }) => (
+                    <span className="text-white text-4xl font-bold">
+                      {remainingTime}
+                    </span>
+                  )}
+                </CountdownCircleTimer>
+              </div>
+            )}
+            {recordingState === "recording" && (
+              <div
+                className="absolute top-px right-px flex space-x-4"
+                onClick={() => {
+                  setRecordingState("stopped");
+                }}
+              >
+                <div>
+                  <span className="text-white">stop recording</span>
+                </div>
+                <span className="text-white">{formatTime(seconds)}</span>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  console.log(contents);
+
   return (
-    <div className="bg-bg-new w-screen h-screen p-4 py-8 flex flex-col space-y-4 pb-18">
+    <div className="bg-bg-new w-screen h-screen p-4 py-8 flex flex-col space-y-4 pb-18 overflow-y-hidden">
       {/* <div className="w-full flex justify-center">
         <div className="border border-task-btn px-3 rounded-full w-fit h-fit">
           <span className="text-md text-task-btn">Task of the day</span>
         </div>
       </div> */}
       <Wrapper
-        step={1}
-        description={
-          <span className=" text-pnpl">
-            If you could change the color of the sky and the grass to{" "}
-            <span className="font-semibold">how you felt today</span> how would
-            it look like?
-          </span>
-        }
-        contents={
-          <div className="">
-            <div className="rounded-t-2xl flex flex-col space-y-4">
-              <div
-                style={{
-                  backgroundColor: colors?.skyColor
-                    ? buildRGBA(colors.skyColor)
-                    : "#fff",
-                }}
-              >
-                <img className="w-full" src="/sky2.png" />
-              </div>
-              <div className="" style={{ marginTop: "-25px" }}>
-                <Grass
-                  color={
-                    colors?.grassColor ? buildRGBA(colors.grassColor) : "#fff"
-                  }
-                />
-              </div>
-
-              <>
-                <p className="text-pnpl text-sm">
-                  Use the slider below to make your selection
-                </p>
-
-                <Tabs
-                  onColorChange={(colors) => {
-                    setColors(colors);
-                  }}
-                  skyColor={colors?.skyColor}
-                  grassColor={colors?.grassColor}
-                />
-              </>
-            </div>
-          </div>
-        }
+        step={task === "color" ? 1 : 2}
+        description={description}
+        contents={contents}
       />
-      <div className="w-full flex justify-center pb-12">
+      <div className="w-full flex justify-center pb-8">
         <button
           className="bg-pnpl text-white font-bold py-2 px-4 rounded-full text-md"
           onClick={sendButton}
