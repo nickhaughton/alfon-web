a641aae182ecbc15a4b5580023562d2a79bbf2c8
nickhaughton <nick@openindsutry.io>
2023-12-12 10:53:15 -0500

    button text

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -59,7 +59,7 @@ export function Tabs({
               className={classNames(
                 tab.name === currentColor
                   ? "bg-pnpl text-white"
-                  : "text-gray-500 hover:text-gray-700",
+                  : "text-pnpl hover:text-pnpl",
                 "rounded-md px-2 py-2 text-sm font-medium uppercase"
               )}
               onClick={() => {
